338a1f603df06468963e4cc2642ce62a6161c41b
brendagelvez <brendagelvez@gmail.com>
2017-10-05 08:31:57 -0500

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,182 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Free Leoshop Website Template | Home :: w3layouts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="./css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="./css/form.css" rel="stylesheet" type="text/css" media="all" />
+    <link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="./js/jquery1.min.js"></script>
+    <!-- start menu -->
+    <link href="./css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+    <script type="text/javascript" src="./js/megamenu.js"></script>
+    <script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+    <!--start slider -->
+    <link rel="stylesheet" href="./css/fwslider.css" media="all">
+    <script src="./js/jquery-ui.min.js"></script>
+    <script src="./js/css3-mediaqueries.js"></script>
+    <script src="./js/fwslider.js"></script>
+    <!--end slider -->
+    <script src="./js/jquery.easydropdown.js"></script>
+</head>
+<body>
+<div class="header-top">
+    <div class="wrap">
+        <div class="header-top-left">
+            <div class="box">
+                <select tabindex="4" class="dropdown">
+                    <option value="" class="label" value="">Language :</option>
+                    <option value="1">English</option>
+                    <option value="2">French</option>
+                    <option value="3">German</option>
+                </select>
+            </div>
+            <div class="box1">
+                <select tabindex="4" class="dropdown">
+                    <option value="" class="label" value="">Currency :</option>
+                    <option value="1">$ Dollar</option>
+                    <option value="2">€ Euro</option>
+                </select>
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div class="cssmenu">
+            <ul>
+                <li class="active"><a href="login.php">Account</a></li> |
+                <li><a href="checkout.php">Wishlist</a></li> |
+                <li><a href="checkout.php">Checkout</a></li> |
+                <li><a href="login.php">Log In</a></li> |
+                <li><a href="register.php">Sign Up</a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="header-bottom">
+    <div class="wrap">
+        <div class="header-bottom-left">
+            <div class="logo">
+                <a href="index.php"><img src="./images/logo.png" alt=""/></a>
+            </div>
+            <div class="menu">
+                <ul class="megamenu skyblue">
+                    <li class="active grid"><a href="index.php">Home</a></li>
+                    <li><a class="color4" href="womens.php">women</a>
+                        <div class="megapanel">
+                            <div class="row">
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Contact Lenses</h4>
+                                        <ul>
+                                            <li><a href="womens.php">Daily-wear soft lenses</a></li>
+                                            <li><a href="womens.php">Extended-wear</a></li>
+                                            <li><a href="womens.php">Lorem ipsum </a></li>
+                                            <li><a href="womens.php">Planned replacement</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Sun Glasses</h4>
+                                        <ul>
+                                            <li><a href="womens.php">Heart-Shaped</a></li>
+                                            <li><a href="womens.php">Square-Shaped</a></li>
+                                            <li><a href="womens.php">Round-Shaped</a></li>
+                                            <li><a href="womens.php">Oval-Shaped</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col1">
+                                    <div class="h_nav">
+                                        <h4>Eye Glasses</h4>
+                                        <ul>
+                                            <li><a href="womens.php">Anti Reflective</a></li>
+                                            <li><a href="womens.php">Aspheric</a></li>
+                                            <li><a href="womens.php">Bifocal</a></li>
+                                            <li><a href="womens.php">Hi-index</a></li>
+                                            <li><a href="womens.php">Progressive</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a class="color5" href="mens.php">Men</a>
+                        <div class="megapanel">
+                            <div class="col1">
+                                <div class="h_nav">
+                                    <h4>Contact Lenses</h4>
+                                    <ul>
+                                        <li><a href="mens.html">Daily-wear soft lenses</a></li>
+                                        <li><a href="mens.html">Extended-wear</a></li>
+                                        <li><a href="mens.html">Lorem ipsum </a></li>
+                                        <li><a href="mens.html">Planned replacement</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col1">
+                                <div class="h_nav">
+                                    <h4>Sun Glasses</h4>
+                                    <ul>
+                                        <li><a href="mens.html">Heart-Shaped</a></li>
+                                        <li><a href="mens.html">Square-Shaped</a></li>
+                                        <li><a href="mens.html">Round-Shaped</a></li>
+                                        <li><a href="mens.html">Oval-Shaped</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col1">
+                                <div class="h_nav">
+                                    <h4>Eye Glasses</h4>
+                                    <ul>
+                                        <li><a href="mens.html">Anti Reflective</a></li>
+                                        <li><a href="mens.html">Aspheric</a></li>
+                                        <li><a href="mens.html">Bifocal</a></li>
+                                        <li><a href="mens.html">Hi-index</a></li>
+                                        <li><a href="mens.html">Progressive</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a class="color6" href="other.php">Other</a></li>
+                    <li><a class="color7" href="other.php">Purchase</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="header-bottom-right">
+            <div class="search">
+                <input type="text" name="s" class="textbox" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+                <input type="submit" value="Subscribe" id="submit" name="submit">
+                <div id="response"> </div>
+            </div>
+            <div class="tag-list">
+                <ul class="icon1 sub-icon1 profile_img">
+                    <li><a class="active-icon c1" href="#"> </a>
+                        <ul class="sub-icon1 list">
+                            <li><h3>sed diam nonummy</h3><a href=""></a></li>
+                            <li><p>Lorem ipsum dolor sit amet, consectetuer  <a href="">adipiscing elit, sed diam</a></p></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="icon1 sub-icon1 profile_img">
+                    <li><a class="active-icon c2" href="#"> </a>
+                        <ul class="sub-icon1 list">
+                            <li><h3>No Products</h3><a href=""></a></li>
+                            <li><p>Lorem ipsum dolor sit amet, consectetuer  <a href="">adipiscing elit, sed diam</a></p></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="last"><li><a href="#">Cart(0)</a></li></ul>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
